8168ec957634540533b3dedab93402e8a061b044
Joao <joao@MacBook-Air-de-Joao.local>
2022-05-25 19:28:16 -0300

    Map com pontos adicionados

--- src/pages/maps/map.js
+++ src/pages/maps/map.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
-import Search from "./search";
 
-let service;
 const google = window.google;
 const libraries = ["places"];
 
@@ -19,6 +17,21 @@ const center = {
   lng: -46.693256,
 };
 
+let points = [
+  {
+    lat: -23.635980, 
+    lng: -46.713319,
+  },
+  {
+    lat: -23.646566, 
+    lng: -46.709545,
+  },
+  {
+    lat: -23.649390, 
+    lng: -46.711507,
+  },
+];
+
 export default function App() {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyCBA4Lm2POl7b0LEF1n2H9-wH4uFC8Xo2Y",
@@ -27,37 +40,44 @@ export default function App() {
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback(map => {
     mapRef.current = map;
-  }, []);
-
-  const panTo = React.useCallback(({ lat, lng }) => {
-    mapRef.current.panTo({ lat, lng });
-    mapRef.current.setZoom(15);
-    let map = mapRef.current;
-
-    let request = {
-      location: { lat, lng },
-      radius: "500",
-      type: ["hospital"]
-    };
-
-    service = new google.maps.places.PlacesService(mapRef.current);
-    service.nearbySearch(request, callback);
-    function callback(results, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (let i = 0; i < results.length; i++) {
-          let place = results[i];
-          new google.maps.Marker({
-            position: place.geometry.location,
-            map
-          });
-        }
-      }
+    for (let i = 0; i < points.length; i++) {
+      new google.maps.Marker({
+        position: points[i],
+        map: map,
+      });
     }
   }, []);
 
+  
+  // const panTo = React.useCallback(({ lat, lng }) => {
+  //   mapRef.current.panTo({ lat, lng });
+  //   mapRef.current.setZoom(15);
+  //   let map = mapRef.current;
+
+  //   let request = {
+  //     location: { lat, lng },
+  //     radius: "500",
+  //     type: ["hospital"]
+  //   };
+
+  //   service = new google.maps.places.PlacesService(mapRef.current);
+  //   service.nearbySearch(request, callback);
+  //   function callback(results, status) {
+  //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+  //       for (let i = 0; i < results.length; i++) {
+  //         let place = results[i];
+  //         new google.maps.Marker({
+  //           position: place.geometry.location,
+  //           map
+  //         });
+  //       }
+  //     }
+  //   }
+  // }, []);
+
   return (
     <div>
-      <Search panTo={panTo} />
+      {/* <Search panTo={panTo} /> */}
       {isLoaded ? (
         <GoogleMap
           id="map"
