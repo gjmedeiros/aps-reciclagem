e72da01b777df5621732d571aaddd3610ac9b320
Gabriel Medeiros <gabriel.m.goncalves@outlook.com>
2022-05-25 21:17:42 -0300

    mudancas no front

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,14 +1,14 @@
 import { Button } from "react-bootstrap";
-import Map from "../maps/map"
+import Map from "../maps/map";
 import "./styles.css";
 
 function Landing() {
-  
   return (
     <div id="landing" className="container">
       <div className="block-grid">
         <div className="card-item">
           <Button>Impacto no meio Ambiente</Button>
+
           <div className="text-impact">
             <p>
               Devemos reciclar para a preservação da biodiversidade; Reciclar
@@ -44,25 +44,37 @@ function Landing() {
           <Button>Por que reciclar ?</Button>
           <div className="what-impact">
             <p>
-              Devemos reciclar para a preservação da biodiversidade; Reciclar
-              implica em gerar menos lixo, menos esgoto, e assim contribuir para
-              um ambiente mais sadio; Cada litro de óleo despejado no esgoto tem
-              capacidade para poluir cerca de 1 milhão de litros de
-              água(SABESP); <br /> O óleo impermeabiliza o solo podendo
+              É de extrema importância reciclar o óleo de cozinha, pois a
+              capacidade poluente da substância é muito alta: apenas um litro de
+              óleo é capaz de poluir cerca de um milhão de litros de água, ou
+              seja, o equivalente a quantidade média consumida por uma pessoa
+              durante 14 anos.
+              <br />A poluição causada óleo na água prejudica a oxigenação,
+              matando plantas e peixes na região ou deixando-os doentes.
+              <br />
+              Se o óleo for despejado diretamente no esgoto, ele encarece o
+              tratamento deste em cerca de 45%. Além de todos esses problemas,
+              ele ainda causa mau cheiro e pode provocar entupimento, ajudando a
+              causar refluxos e alagamentos.
+              <br />
+              Para evitar todos esses problemas é só adotar uma postura mais
+              consciente, evitando usar óleo desnecessariamente e coletando o
+              mesmo para a reciclagem. O ideal é colocá-lo em uma garrafa de
+              plástico transparente, e entregar em algum posto de coleta na
+              cidade.
             </p>
           </div>
         </div>
       </div>
 
       <div className="text-center">
-        <strong>Qual o método ideal para descarte?</strong>
+        <strong>Pontos de Coleta</strong>
       </div>
       <div className="map-google" id="map">
-        <Map/>
+        <Map />
       </div>
     </div>
   );
 }
 
 export default Landing;
-
